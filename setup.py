e63781204a0a517a850fcdcd0e2282812c4b7f9e
Kris Steinhoff <ksteinhoff@gmail.com>
2023-02-20 11:41:44 -0500

    Bump version: 0.1.22 → 0.1.23

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dbcl",
-    version="0.1.22",
+    version="0.1.23",
     description="A database command line interface that is engine-agnostic.",
     author="Kris Steinhoff",
     url="https://github.com/ksofa2/dbcl",
